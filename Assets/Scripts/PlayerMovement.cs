e49bca123fb3920bc47068e6124cbfd70ea0de4d
FricativeMelon <43120800+FricativeMelon@users.noreply.github.com>
2022-01-19 11:09:50 -0500

    Added collision and tiles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    CharacterController controller;
+    Rigidbody2D controller;
     public float speed = 3f;
+    public float health = 80;
     public Camera playerCamera;
     public LayerMask enemy;
     public Transform punchOrigin;
     // Start is called before the first frame update
     void Start()
     {
-        controller=GetComponent<CharacterController>();
+        controller=GetComponent<Rigidbody2D>();
         playerCamera=Camera.main;
     }
 
@@ -20,12 +21,25 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector2 velocity=new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
-        controller.Move(velocity*Time.deltaTime*speed);
+        controller.AddForce(1000*velocity * Time.deltaTime);
         punchOrigin.LookAt(playerCamera.ScreenToWorldPoint(Input.mousePosition));
         if(Input.GetMouseButtonDown(0)){
             punch();
         }
     }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
     void punch(){
         Vector2 mousePos=Input.mousePosition;
         RaycastHit2D hit=Physics2D.Raycast(transform.position, punchOrigin.right, 1, enemy);
