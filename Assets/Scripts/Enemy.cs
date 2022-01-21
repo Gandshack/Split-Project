613f4559211f3b60735252fbf44269875d964ea6
lunispang <95270420+lunispang@users.noreply.github.com>
2022-01-21 19:19:26 +0100

    minor changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour
     {
         playerDistance = Mathf.Sqrt(Mathf.Pow((player.transform.position.x - transform.position.x),2) + Mathf.Pow((player.transform.position.y - transform.position.y),2));
 
-        if ((playerMovement.WeaponOut == true) && playerDistance < 10f)
+        if ((playerMovement.WeaponOut == true) && playerDistance < 5f)
         {
             TakeDamage(100/playerDistance * Time.deltaTime);
         }
