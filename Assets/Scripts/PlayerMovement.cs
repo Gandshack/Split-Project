c5f05e7e5135387cc1ab20005e4fcda42e119771
FricativeMelon <43120800+FricativeMelon@users.noreply.github.com>
2022-01-26 15:49:51 -0500

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -99,32 +99,6 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    /*bool OnSlope()
-     {
-         Rect r = isGroundedChecker.rect;
-         float y = isGroundedChecker.position.y;
-         float x1 = isGroundedChecker.position.x - r.size.x/2;
-         float x2 = isGroundedChecker.position.x + r.size.x / 2;
-         RaycastHit2D hit = Physics2D.Raycast(new Vector2(x1, y), new Vector2(0, -1), 0.5f, groundLayer);
-         RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(x1+0.01f, y), new Vector2(0, -1), 0.5f, groundLayer);
-         RaycastHit2D hit3 = Physics2D.Raycast(new Vector2(x2-0.01f, y), new Vector2(0, -1), 0.5f, groundLayer);
-         RaycastHit2D hit4 = Physics2D.Raycast(new Vector2(x2, y), new Vector2(0, -1), 0.5f, groundLayer);
-
-         if (hit && hit2)
-         {
-             Debug.Log(hit.distance);
-             Debug.Log(hit2.distance);
-             return Math.Min(hit.distance, hit2.distance) < 0.2f && Math.Abs(hit.distance - hit2.distance) > 0.005f;
-         }
-         if (hit3 && hit4)
-         {
-             Debug.Log(hit3.distance);
-             Debug.Log(hit4.distance);
-             return Math.Min(hit3.distance, hit4.distance) < 0.2f && Math.Abs(hit3.distance - hit4.distance) > 0.005f;
-         }
-         return false;
-     }*/
-
     bool OnGround()
     {
         Bounds r = playerCollider.bounds;
@@ -271,19 +245,5 @@ public class PlayerMovement : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
         Debug.Log("player ded");
-    }
-
-    bool CheckIfSided(RectTransform isChecker)
-    {
-        Rect r = isChecker.rect;
-        Collider2D collider = Physics2D.OverlapBox(isChecker.position, r.size, 0, groundLayer);
-        if (collider != null)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 }
