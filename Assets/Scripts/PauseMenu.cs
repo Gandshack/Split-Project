d165a3b50cbe13f6256ea448b417795add23ff2c
FricativeMelon <43120800+FricativeMelon@users.noreply.github.com>
2022-02-12 11:31:41 -0500

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class PauseMenu : MonoBehaviour
+    {
+        private bool paused;
+        private Transform menu;
+
+        void Start()
+        {
+            paused = false;
+            menu = transform.Find("Menu");
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                if (paused)
+                {
+                    Time.timeScale = 1;
+                    paused = false;
+                    menu.gameObject.SetActive(false);
+                }
+                else
+                {
+                    Time.timeScale = 0;
+                    paused = true;
+                    menu.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
